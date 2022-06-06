f72f75a9d179b76f507fb604568b272fe3c09e31
kajela <titoyut56@gmail.com>
2022-06-06 16:08:03 -0400

    add the initial files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,13 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+public class Main {
+    public void myBox() {
+        System.out.println("its a double cabinet");
+    }
+    public  void mySpeaker(int price){
+        System.out.println("its a T-s 600D2 " +price);
+    }
+    public void myWoman(){
+        System.out.println("just for the streets");
     }
 }
--- src/main/java/org/example/Second.java
+++ src/main/java/org/example/Second.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class Second {
+    public static void main(String[] args) {
+        Main myObj = new Main();
+        myObj.mySpeaker(3000);
+       myObj.myBox();
+       myObj.myWoman();
+    }
 }
